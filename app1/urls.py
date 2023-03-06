9c41370606390688b00c8e664d152c30c53b86dd
devvlopar <devangsingh34@gmail.com>
2023-03-06 15:50:17 +0530

    AJAX

---
+++ app1/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [ 
+    path('', index, name='index'),
+    path('create_row/', create_row, name='create_row'),
+    path('del_row/', del_row, name='del_row'),
+
+
+]
